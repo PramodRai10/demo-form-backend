cd637c8364acbce0d49431aaf22d9923b9b5f0c3
PramodRai10 <p10pramodr@gmail.com>
2021-03-25 17:45:12 +0530

    First Commit

---
+++ drive.js
@@ -0,0 +1,16 @@
+const { google } = require('googleapis');
+const credentials = require('./credentials.json');
+const fs = require('fs')
+
+// Code to upload the file
+
+const scopes = [
+  'https://www.googleapis.com/auth/drive'
+];
+const auth = new google.auth.JWT(
+  credentials.client_email, null,
+  credentials.private_key, scopes
+);
+const drive = google.drive({ version: "v3", auth });
+
+module.exports = drive;
